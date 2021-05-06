aae39f89541e5532db4b9811098771f0be50ac1b
vchemiere <vchemiere@viggor.eu>
2021-05-06 14:15:28 +0200

    add page

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/modis")
+def modis():
+    return "<iframe width="560" height="315" src="https://www.youtube.com/embed/4hzGMqk0-us" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"
