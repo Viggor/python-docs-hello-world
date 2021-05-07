4a7fda8d5d95d75000f10357d5c9eafbcacc4f90
vchemiere <vchemiere@viggor.eu>
2021-05-07 11:38:19 +0200

    change route

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<a href='/modis'>Modis Video</a><br/><a href='/user-agent'>Mon user-agent</a>"
+    return "<a href='/modis'>Modis Video</a><br/><a href='/useragent'>Mon user-agent</a>"
 
 @app.route("/modis")
 def modis():
     return '<iframe width="560" height="315" src="https://www.youtube.com/embed/4hzGMqk0-us" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 
-@app.route("/user-agent")
+@app.route("/useragent")
 def useragent():
     template = "mon user agent est {{ ua }}"
     data = {
