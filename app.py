10a08cfbd7eb51386b85a422635958fd945cb55d
vchemiere <vchemiere@viggor.eu>
2021-05-07 11:35:00 +0200

    add page with jinja

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask import Request
+from jinja2 import Template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<a href='/modis'>Modis Video</a>"
+    return "<a href='/modis'>Modis Video</a><br/><a href='/user-agent'>Mon user-agent</a>"
 
 @app.route("/modis")
 def modis():
     return '<iframe width="560" height="315" src="https://www.youtube.com/embed/4hzGMqk0-us" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+@app.route("/user-agent")
+def useragent():
+    template = "mon user agent est {{ ua }}"
+    data = {
+        "ua": Request.headers.get('User-Agent')
+    }
+    jtemplate = Template(template)
+    return jtemplate.render(data)
